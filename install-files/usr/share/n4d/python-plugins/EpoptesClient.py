e55e1e98839d6993d1428528cec44869e48b83be
hectorgh <hectorgh@gmail.com>
2019-01-15 09:30:27 +0100

    adding source files

---
+++ install-files/usr/share/n4d/python-plugins/EpoptesClient.py
@@ -0,0 +1,65 @@
+import subprocess
+import os
+import lliurex.net
+import threading
+
+class EpoptesClient:
+	
+	def startup(self,options):
+		
+		if options["controlled"]:
+			if options["boot"]:
+				self.grab_epoptes_certificate_thread()
+				self.disable_wol_thread()
+					
+	#def startup
+	
+	def grab_epoptes_certificate_thread(self):
+		
+		t=threading.Thread(target=self._grab_epoptes_certificate)
+		t.daemon=True
+		t.start()
+		
+	#def epoptes_daemon
+	
+	def _grab_epoptes_certificate(self):
+		
+		if os.system("host server > /dev/null")==0:
+			
+			execute=False
+			p=subprocess.Popen(["ps aux | grep 'epoptes-client -c' | wc -l"],shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+			output=p.communicate()
+
+			try:
+				t=int(output[0].strip("\n"))
+				if t<=2:
+					execute=True
+			except:
+				execute=False
+				
+			
+			if execute:
+				p=subprocess.Popen(["epoptes-client -c"],shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+				p.communicate()
+		
+	#def _grab_epoptes_certificate
+	
+	def disable_wol_thread(self):
+		
+		t=threading.Thread(target=self._disable_wol)
+		t.daemon=True
+		t.start()
+		
+	#def disable_wol_thread
+	
+	
+	def  _disable_wol(self):
+		
+		devices=lliurex.net.get_devices_info()
+		for dev in devices:
+			os.system("ethtool -s %s wol g"%dev["name"])
+		
+	#def _disable_wol
+	
+#class EpoptesClient
+
